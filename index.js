164f027ac4e07c38f04c0381c653b9ed1d8948ed
Loli Killers <80396688+LoliKillers@users.noreply.github.com>
2021-08-24 10:03:30 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+var express = require('express'),
+    cors = require('cors'),
+    secure = require('ssl-express-www');
+const PORT = process.env.PORT || 8080 || 5000 || 3000
+var { color } = require('./lib/color.js')
+
+var mainrouter = require('./routes/main'),
+    apirouter = require('./routes/api')
+
+var app = express()
+app.enable('trust proxy');
+app.set("json spaces",2)
+app.use(cors())
+app.use(secure)
+app.use(express.static("assets"))
+
+app.use('/', mainrouter)
+app.use('/docs', apirouter)
+
+app.listen(PORT, () => {
+    console.log(color("Server running on port " + PORT,'green'))
+})
+
+module.exports = app
